22b186e0adb3a30db0525aa4cc3c435c75cd3c9e
Justin W Soto <soto@pdx.edu>
2021-09-27 14:52:01 -0700

    started adding info to table

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,11 +2,19 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import QuotesModel from '../lib/models/QuotesModel.js';
 
-describe('demo routes', () => {
+
+describe('routes to get, post, put, and delete', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('POSTs a new quote to the table', async () => {
+    const quote = {
+      
+    }
+  })
 
   afterAll(() => {
     pool.end();
